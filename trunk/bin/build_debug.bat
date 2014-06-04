1d593161258f2dc0805b1271c34b546ef0722553
leezhisz@gmail.com <leezhisz@gmail.com@117638b5-b9ed-77bc-84b0-4419d61a5df1>
2014-06-04 04:54:19 +0000

    remove build cx_image project

--- trunk/bin/build_debug.bat
+++ trunk/bin/build_debug.bat
@@ -1,16 +1,4 @@
 
-del ui.log 2> nul
-echo  ±‡“Î ui_cximage.sln
-call "%VS90COMNTOOLS%..\IDE\devenv.exe" "%~0\..\..\code\src\ui_cximage.sln" /build debug  /out "%~0\..\ui_cximage.log"
-type %~0\..\ui_cximage.log
-echo off
-set LOGFILE=%~0\..\ui_cximage.log
-findstr /C:" ß∞‹ 0 ∏ˆ" "%LOGFILE%"
-if %ERRORLEVEL% NEQ 0 (
-	pause & exit
-)
-del %~0\..\ui_cximage.log
-
 echo  ±‡“Î UI.sln
 call "%VS90COMNTOOLS%..\IDE\devenv.exe" "%~0\..\..\code\src\UI.sln" /build debug  /out "%~0\..\ui.log"
 type %~0\..\ui.log
