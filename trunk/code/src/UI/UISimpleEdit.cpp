58c42b029c1d41e6b7bbf329d09d3e795405e570
leezhisz@gmail.com <leezhisz@gmail.com@117638b5-b9ed-77bc-84b0-4419d61a5df1>
2014-07-03 16:03:44 +0000

    edit align

--- trunk/code/src/UI/UISimpleEdit.cpp
+++ trunk/code/src/UI/UISimpleEdit.cpp
@@ -4,6 +4,11 @@
 CUISimpleEdit::CUISimpleEdit(void)
 {
 	m_bPassword = FALSE;
+	m_bMultiLine = FALSE;
+	m_bWordBreak = FALSE;
+	m_bWordEllipsis = FALSE;
+	m_evalign = EVA_TOP;
+	m_ehalign = EHA_LEFT;
 	RegisterClass(this);
 }
 
@@ -16,6 +21,11 @@ CUISimpleEdit::CUISimpleEdit(CUITreeContainer* pTree, LPXMLDOMNode pNode)
 : CUIControlBase(pTree, pNode)
 {
 	m_bPassword = FALSE;
+	m_bMultiLine = FALSE;
+	m_bWordBreak = FALSE;
+	m_bWordEllipsis = FALSE;
+	m_evalign = EVA_TOP;
+	m_ehalign = EHA_LEFT;
 	RegisterClass(this);
 	if(pNode == NULL || pNode->pMapAttr == NULL)
 		return;
@@ -56,11 +66,69 @@ CUISimpleEdit::CUISimpleEdit(CUITreeContainer* pTree, LPXMLDOMNode pNode)
 		std::string strPwd = it2->second->strUData;
 		m_bPassword = (strPwd == "1" || strPwd == "true");
 	}
+	it2 = pAttrChildNodes->find("valign");
+	if (it2 != pAttrChildNodes->end())
+	{
+		std::string strVAlign = it2->second->strUData;
+		if (strVAlign == "top")
+		{
+			m_evalign = EVA_TOP;
+		}
+		else if (strVAlign == "center")
+		{
+			m_evalign = EVA_CENTER;
+		}
+		else if (strVAlign == "bottom")
+		{
+			m_evalign = EVA_BOTTOM;
+		}
+	}
+	it2 = pAttrChildNodes->find("halign");
+	if (it2 != pAttrChildNodes->end())
+	{
+		std::string strHAlign = it2->second->strUData;
+		if (strHAlign == "left")
+		{
+			m_ehalign = EHA_LEFT;
+		}
+		else if (strHAlign == "center")
+		{
+			m_ehalign = EHA_CENTER;
+		}
+		else if (strHAlign == "right")
+		{
+			m_ehalign = EHA_RIGHT;
+		}
+	}
+	it2 = pAttrChildNodes->find("wordbreak");
+	if (it2 != pAttrChildNodes->end())
+	{
+		std::string strWordBreak = it2->second->strUData;
+		m_bWordBreak = (strWordBreak == "1" || strWordBreak == "true");
+	}
+	it2 = pAttrChildNodes->find("wordellipsis");
+	if (it2 != pAttrChildNodes->end())
+	{
+		std::string strWordEllipsis = it2->second->strUData;
+		m_bWordEllipsis = (strWordEllipsis == "1" || strWordEllipsis == "true");
+	}
+	it2 = pAttrChildNodes->find("multiline");
+	if (it2 != pAttrChildNodes->end())
+	{
+		std::string strMultiLine = it2->second->strUData;
+		m_bMultiLine = (strMultiLine == "1" || strMultiLine == "true");
+	}
 }
 
 CUISimpleEdit::CUISimpleEdit(CUITreeContainer* pTree)
 : CUIControlBase(pTree)
 {
+	m_bPassword = FALSE;
+	m_bMultiLine = FALSE;
+	m_bWordBreak = FALSE;
+	m_bWordEllipsis = FALSE;
+	m_evalign = EVA_TOP;
+	m_ehalign = EHA_LEFT;
 	RegisterClass(this);
 }
 
@@ -78,8 +146,7 @@ void CUISimpleEdit::Render(CDCHandle dc)
 	{
 		pColor->SelectObject(dc);
 	}
-	dc.DrawText(m_strText.c_str(), -1, &rc, DT_CALCRECT | DT_SINGLELINE | DT_LEFT | DT_VCENTER);
-	dc.DrawText(m_strText.c_str(), -1, &rc, DT_SINGLELINE | DT_LEFT | DT_VCENTER);
+	dc.DrawText(m_strText.c_str(), -1, &rc, GetFormat());
 	if (pFont)
 	{
 		pFont->RestoreObject(dc);
@@ -126,27 +193,89 @@ void CUISimpleEdit::SetAttr(const std::string& strName, const std::string& strVa
 void CUISimpleEdit::CaclTextRect(RECT& rc)
 {
 	rc = GetObjPos();
-	CUIWindowBase* pWindow = GetOwnerTree()->GetBindWnd();
-	CDCHandle dc = ::GetDC(pWindow->m_hWnd);
-	CUIFont* pFont = UIResFactoryInstance->GetFont(m_strFontID.c_str());
-	if (pFont)
+	LONG lnFormat = GetFormat();
+	if (lnFormat & DT_SINGLELINE)
 	{
-		pFont->SelectObject(dc);
+		LONG lnWidth = 0, lnHeight = 0;
+		GetTextExtent(lnWidth, lnHeight);
+		if (lnFormat & DT_LEFT)
+		{
+			rc.right = rc.left + lnWidth;
+		}
+		else if (lnFormat & DT_RIGHT)
+		{
+			rc.left = rc.right - lnWidth;
+		}
+		else if (lnFormat & DT_CENTER)
+		{
+			rc.left = rc.left + (rc.right - rc.left - lnWidth) / 2;
+			rc.right = rc.left + lnWidth;
+		}
+
+		if (lnFormat & DT_TOP)
+		{
+			rc.bottom = rc.top + lnHeight;
+		}
+		else if (lnFormat & DT_BOTTOM)
+		{
+			rc.top = rc.bottom - lnHeight;
+		}
+		else if (lnFormat & DT_VCENTER)
+		{
+			rc.top = rc.top + (rc.bottom - rc.top- lnHeight) / 2;
+			rc.bottom = rc.top + lnHeight;
+		}
 	}
-	CUIColor* pColor = UIResFactoryInstance->GetColor(m_strColorID.c_str());
-	if (pColor)
+	else
 	{
-		pColor->SelectObject(dc);
+
 	}
-	dc.DrawText(m_strText.c_str(), -1, &rc, DT_CALCRECT | DT_SINGLELINE | DT_LEFT | DT_VCENTER);
-	if (pFont)
+}
+
+LONG CUISimpleEdit::GetFormat()
+{
+	LONG lnFormat = 0;
+	if (!m_bMultiLine)
 	{
-		pFont->RestoreObject(dc);
+		lnFormat |= DT_SINGLELINE;
 	}
-	if (pColor)
+	if (m_bWordBreak)
 	{
-		pColor->RestoreObject(dc);
+		lnFormat |= DT_WORDBREAK;
 	}
+	if (m_bWordEllipsis)
+	{
+		lnFormat |= DT_WORD_ELLIPSIS;
+	}
+	switch (m_ehalign)
+	{
+	case EHA_CENTER:
+		lnFormat |= DT_CENTER;
+		break;
+	case EHA_LEFT:
+		lnFormat |= DT_LEFT;
+		break;
+	case EHA_RIGHT:
+		lnFormat |= DT_RIGHT;
+		break;
+	default:
+		break;
+	}
+	switch (m_evalign)
+	{
+	case EVA_CENTER:
+		lnFormat |= DT_VCENTER;
+		break;
+	case EVA_TOP:
+		lnFormat |= DT_TOP;
+		break;
+	case EVA_BOTTOM:
+		lnFormat |= DT_BOTTOM;
+		break;
+	default:
+		break;
+	}
+	return lnFormat;
 }
 
 void CUISimpleEdit::SetVisible(BOOL bVisible)
@@ -180,6 +309,46 @@ void CUISimpleEdit::OnLButtonDown(int x, int y)
 {
 	RECT rc = {0};
 	CaclTextRect(rc);
+	CUIWindowBase* pWindow = GetOwnerTree()->GetBindWnd();
+	CDCHandle dc = ::GetDC(pWindow->m_hWnd);
+	CUIFont* pFont = UIResFactoryInstance->GetFont(m_strFontID.c_str());
+	if (pFont)
+	{
+		pFont->SelectObject(dc);
+	}
+	CUIColor* pColor = UIResFactoryInstance->GetColor(m_strColorID.c_str());
+	if (pColor)
+	{
+		pColor->SelectObject(dc);
+	}
+	LONG lnLeft = rc.left;
+	for (size_t i = 0; i < m_strText.length(); i++)
+	{
+		SIZE sz = {0};
+		dc.GetTextExtent(&m_strText[i], 1, &sz);
+		if (lnLeft + sz.cx < x)
+		{
+			lnLeft += sz.cx;
+			continue;
+		}
+		if ((x - lnLeft) <= sz.cx / 2)
+		{
+			rc.left = lnLeft;
+		}
+		else
+		{
+			rc.left = lnLeft + sz.cx;
+		}
+		break;
+	}
+	if (pFont)
+	{
+		pFont->RestoreObject(dc);
+	}
+	if (pColor)
+	{
+		pColor->RestoreObject(dc);
+	}
 	::SetCaretPos(rc.left, rc.top);
 }
 
@@ -224,6 +393,11 @@ std::string CUISimpleEdit::SetTextColor(const std::string& strTextColor)
 
 void CUISimpleEdit::GetTextExtent(LONG& lnWidth, LONG& lnHeight)
 {
+	GetTextExtent(m_strText.c_str(), lnWidth, lnHeight);
+}
+
+void CUISimpleEdit::GetTextExtent(const wchar_t* psz, LONG& lnWidth, LONG& lnHeight)
+{
 	CUIWindowBase* pWindow = GetOwnerTree()->GetBindWnd();
 	CDCHandle dc = ::GetDC(pWindow->m_hWnd);
 	CUIFont* pFont = UIResFactoryInstance->GetFont(m_strFontID.c_str());
@@ -237,7 +411,7 @@ void CUISimpleEdit::GetTextExtent(LONG& lnWidth, LONG& lnHeight)
 		pColor->SelectObject(dc);
 	}
 	SIZE sz = {0};
-	dc.GetTextExtent(m_strText.c_str(), -1, &sz);
+	dc.GetTextExtent(psz, -1, &sz);
 	if (pFont)
 	{
 		pFont->RestoreObject(dc);
